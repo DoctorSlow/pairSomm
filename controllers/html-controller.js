9f8cd2979028d10e44e8f6552cd28a630dffe1bf
Jeremy J. Lee <doctorslow@gmail.com>
2018-05-06 19:05:42 -0700

    updated routes to controllers, edited express to render handlebars

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -8,22 +8,24 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  app.get("/", function (req, res) {
+    res.render("index");
   });
 
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  app.get("/login", function (req, res) {
+    res.render("index", {
+      layout: "login"
+    });
   });
 
   // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  app.get("/profile", function (req, res) {
+    res.render("profile");
   });
 
 };
--- server.js
+++ server.js
@@ -17,8 +17,18 @@ var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // parse application/json
 app.use(bodyParser.json());
 
@@ -27,13 +37,19 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+require("./controllers/api-controller.js")(app);
+require("./controllers/html-controller.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// db.sequelize.sync({
+//   force: true
+// }).then(function () {
+//   app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
