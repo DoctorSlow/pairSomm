acf198bdffb8ff35e0250623c68b7eb98f4e2bb8
Ryan <ryetoaster@gmail.com>
2018-05-10 20:20:37 -0700

    working on passport

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
+
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS blogger;
+DROP DATABASE IF EXISTS userdb;
 -- Creates the "blogger" database --
-CREATE DATABASE blogger;
+CREATE DATABASE userdb;
+
