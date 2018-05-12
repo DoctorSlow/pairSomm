0da6decde95f5be784d706869e37221291fe03e4
emaunevans <32587296+emaunevans@users.noreply.github.com>
2018-05-12 14:00:03 -0700

    browse test

--- public/assets/js/browse.js
+++ public/assets/js/browse.js
@@ -1,1 +1,13 @@
-//GET data from db, establish UPDATE for voting
+$(".btn-danger").attr ("onclick","downvote()"); 
+$(".btn-success").attr ("onclick","upvote()"); 
+app.put("/api/posts", function(req, res) {
+  db.Post.update(req.body,
+    {
+      where: {
+        id: req.body.id
+      }
+    })
+    .then(function(userdb) {
+      res.json(userdb);
+    });
+});
