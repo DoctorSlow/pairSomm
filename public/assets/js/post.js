bad1cbcf8759626c8c0486b6d983ed60beb815eb
Ryan <ryetoaster@gmail.com>
2018-05-13 20:43:24 -0700

    fix button

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -3,3 +3,17 @@ $(document).ready(function () {
 
 
 });
+function upvote(todo) {
+    $.ajax({
+        method: "PUT",
+        url: "/post/up",
+        data: todo
+    }).then(getTodos);
+}
+function downvote(todo) {
+    $.ajax({
+        method: "PUT",
+        url: "/post/down",
+        data: todo
+    }).then(getTodos);
+}
