240e76bfdb1abf5aa6e2157004d5adf9d8a8fd84
Ryan <ryetoaster@gmail.com>
2018-05-13 20:53:31 -0700

    fixed some forms

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -3,17 +3,3 @@ $(document).ready(function () {
 
 
 });
-function upvote(todo) {
-    $.ajax({
-        method: "PUT",
-        url: "/post/up",
-        data: todo
-    }).then(getTodos);
-}
-function downvote(todo) {
-    $.ajax({
-        method: "PUT",
-        url: "/post/down",
-        data: todo
-    }).then(getTodos);
-}
