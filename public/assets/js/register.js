1578be478f3584b781fab37404b4b4034eb9e03a
Jeremy J. Lee <doctorslow@gmail.com>
2018-05-14 18:46:47 -0700

    fixed modal for terms

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,4 +1,8 @@
-//Get Modal Element 
-var modal=document.getElementById('simpleModal'); 
-//Open modal button 
-var modalBtn=document.getElementById('modalBtn');
+// //Get Modal Element 
+// var modal=document.getElementById('simpleModal'); 
+// //Open modal button 
+// var modalBtn=document.getElementById('modalBtn');
+
+function enableButton2() {
+    document.getElementById("button2").disabled = false;
+}
