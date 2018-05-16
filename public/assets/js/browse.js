6d10334874b8b302ddfef3ad6127dbd8982723cb
emaunevans <32587296+emaunevans@users.noreply.github.com>
2018-05-15 17:46:11 -0700

    test

--- public/assets/js/browse.js
+++ public/assets/js/browse.js
@@ -35,4 +35,22 @@ function upvote() {
         method: "PUT",
         url: "/post/up"
     }).then(upvotes);
-}
+}; 
+
+function hideVote1() {
+        var x = document.getElementById("vote-count1");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }; 
+
+    function hideVote2() {
+            var x = document.getElementById("vote-count2");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        } 
